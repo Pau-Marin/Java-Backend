6e9a15034b822b647866531923cdd9132eacfe5f
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-02-16 11:37:50 +0100

    Create data in database

--- src/main/java/com/paumarin/Main.java
+++ src/main/java/com/paumarin/Main.java
@@ -2,9 +2,7 @@ package com.paumarin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,4 +26,18 @@ public class Main {
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    record NewCustomerRequest(String name, String email, Integer age) {
+    }
+
+
 }
